a1f08bbeaab35f1039b56a603e2f560ad8bb17a4
AdolisDan <adolis.dan@gmail.com>
2024-11-29 15:31:10 +0200

    Create MyLib.h

---
+++ MyLib.h
@@ -0,0 +1,25 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::left;
+using std::right;
+using std::fixed;
+using std::string;
+using std::vector;
+using std::istream;
+using std::ostream;
+using std::sort;
+
+
+#endif
